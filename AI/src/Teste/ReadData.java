6a339eaa3bca0ac2862d2fb3b16274ab3aa42018
SageScroll18144 <ffss@cin.ufpe.br>
2023-09-18 10:17:52 -0300

    implementando comunicação serial

---
+++ AI/src/Teste/ReadData.java
@@ -0,0 +1,62 @@
+package Teste;
+
+import com.fazecast.jSerialComm.SerialPort;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class ReadData {
+    public static void main(String[] args) {
+        SerialPort comPort = SerialPort.getCommPort("ttyACM0");
+
+        comPort.setBaudRate(115200);
+        comPort.setNumDataBits(8);
+        comPort.setParity(SerialPort.NO_PARITY);
+        comPort.setNumStopBits(1);
+
+        char[][] board = new char[8][8];
+
+        if (comPort.openPort()) {
+            System.out.println("Porta serial aberta com sucesso.");
+            comPort.setComPortTimeouts(SerialPort.TIMEOUT_READ_BLOCKING, 1000, 0);
+            BufferedReader in = new BufferedReader(new InputStreamReader(comPort.getInputStream()));
+            while(true) {
+                try {
+                    String msg = in.readLine();
+                    System.out.println(msg);
+
+                    if(msg.length() >= 64){
+                        int idx = 0;
+                        for (int i = 0; i < board.length; i++) {
+                            for (int j = 0; j < board[i].length; j++) {
+                                board[i][j] = msg.charAt(idx);
+                                idx++;
+                            }
+                        }
+                    }
+
+                    System.out.println("\n----------------");
+                    for (int i = 0; i < board.length; i++) {
+                        for (int j = 0; j < board[i].length; j++) {
+                            System.out.print(board[i][j]);
+                        }
+                        System.out.println();
+                    }
+                    System.out.println("\n----------------");
+
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            //comPort.closePort();
+            // Crie uma matriz 8x8 para armazenar os dados
+
+
+        } else {
+            System.err.println("Erro ao abrir a porta serial.");
+        }
+    }
+
+}
--- AI/src/Teste/SerialTest.java
+++ AI/src/Teste/SerialTest.java
@@ -5,7 +5,7 @@ import com.fazecast.jSerialComm.SerialPort;
 public class SerialTest {
     public static void main(String[] args) {
         System.out.println("Hello world");
-
+        
         SerialPort[] ports = SerialPort.getCommPorts();
 
         for (SerialPort port: ports) {
