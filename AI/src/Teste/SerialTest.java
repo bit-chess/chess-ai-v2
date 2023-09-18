a4a74c644aa7ab9fd3bc56a76f74dede21034d4b
SageScroll18144 <ffss@cin.ufpe.br>
2023-09-18 00:18:35 -0300

    implementando jserialcomm

---
+++ AI/src/Teste/SerialTest.java
@@ -0,0 +1,16 @@
+package Teste;
+
+import com.fazecast.jSerialComm.SerialPort;
+
+public class SerialTest {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+
+        SerialPort[] ports = SerialPort.getCommPorts();
+
+        for (SerialPort port: ports) {
+            System.out.println(port.getSystemPortName());
+        }
+    }
+
+}
